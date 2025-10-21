7f3c045a40d3a2789ccc8e176012b21d7df8b64b
Hasan Raisan <contact@example.com>
2025-10-21 10:04:02 +0300

    Convert submodule 'First Windows Service' to normal folder

---
+++ First Windows Service/MyFirstWinService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWinSerivce
+{
+    public partial class MyFirstWinService : ServiceBase
+    {
+        public MyFirstWinService()
+        {
+            InitializeComponent();
+
+            CanPauseAndContinue = true;
+
+            CanShutdown = true;
+
+
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            string logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Service OnStart\n";
+            LogServiceEvent(logMessage);
+        }
+
+        protected override void OnPause()
+        {
+            string logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Service OnPause\n";
+            LogServiceEvent(logMessage);
+        }
+
+        protected override void OnShutdown()
+        {
+            string logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Service OnShutdown\n";
+            LogServiceEvent(logMessage);
+        }
+
+        protected override void OnStop()
+        {
+            string logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Service OnStap\n";
+            LogServiceEvent(logMessage);
+        }
+        protected override void OnContinue()
+        {
+            string logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Service OnContinue\n";
+            LogServiceEvent(logMessage);
+        }
+
+
+        private void InitializLogFile(ref string  logFilePath)
+        {
+            string logDirectory = @"C:\English";
+
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+            logFilePath = Path.Combine(logDirectory, "MyServiceLog.txt");
+
+        }
+        private void LogServiceEvent(string logMessage)
+        {
+            string logFilePath = string.Empty;
+
+            InitializLogFile(ref logFilePath);
+
+            File.AppendAllText(logFilePath, logMessage);
+        }
+
+
+    }
+
+
+}
---
+++ First Windows Service/PojectInstaller.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+
+namespace MyFirstWinSerivce
+{
+    [RunInstaller(true)]
+    public partial class PojectInstaller : Installer
+    {
+        private ServiceProcessInstaller serviceProcessInstaller;
+        ServiceInstaller serviceInstaller;
+        public PojectInstaller()
+        {
+            InitializeComponent();
+
+            // configure the servce
+            serviceProcessInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.NetworkService
+            };
+
+            serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "MyFirstWinService",
+                DisplayName = "My First Windows Service",
+                StartType = ServiceStartMode.Manual
+            };
+            
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(serviceInstaller);
+
+        }
+    }
+}
